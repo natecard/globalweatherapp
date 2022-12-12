3d869dac4497f5f0d01149019907c6fa0a203c79
Nate Card <ncard24@gmail.com>
2022-12-11 20:52:40 -0500

    CSS updated

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-inner-declarations */
 /* eslint-disable no-unused-vars */
-import './style.css';
 import utc from 'dayjs/plugin/utc';
 import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
